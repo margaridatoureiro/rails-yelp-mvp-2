8638791bc36274008b30149bce7be4cbb09bf11e
Margarida Toureiro <margaridatoureiro@gmail.com>
2020-08-04 20:06:48 +0100

    everything looks lovely and is working as intended

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Let us create some restaurants for these curious creatures known as Hobbits'
+
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create(name: Faker::Restaurant.unique.name, address: Faker::Address.unique.street_address, phone_number: Faker::Number.unique.leading_zero_number(digits: 10), category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
+puts 'Done! 10 new dinner place options have been forged in Mount Doom, Mordor'
